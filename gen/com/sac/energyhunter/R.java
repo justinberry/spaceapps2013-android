b97f17ceeaff1ef32ceefed90ab0bfe2e4a31026
Konrad Cybulski <cyb0001@jmss.vic.edu.au>
2013-04-20 22:51:04 +1000

    Update2
    Stat Calculator activity class (sorry for any inefficient and crude
    code, namely all of it).

--- gen/com/sac/energyhunter/R.java
+++ gen/com/sac/energyhunter/R.java
@@ -128,23 +128,42 @@ containing a value of this type.
          */
         public static final int zOrderOnTop=0x7f01000d;
     }
+    public static final class color {
+        public static final int background_semi=0x7f060000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int solar_l1=0x7f020001;
+        public static final int solar_l2=0x7f020002;
+        public static final int solar_l3=0x7f020003;
+        public static final int solar_l4=0x7f020004;
+        public static final int solar_l5=0x7f020005;
+        public static final int solar_l6=0x7f020006;
     }
     public static final class id {
         public static final int hybrid=0x7f040004;
         public static final int map=0x7f040005;
-        public static final int menu_settings=0x7f040006;
+        public static final int map_more_info_btn=0x7f040006;
+        public static final int map_stat_calc=0x7f040008;
+        public static final int menu_settings=0x7f04000e;
         public static final int none=0x7f040000;
         public static final int normal=0x7f040001;
         public static final int satellite=0x7f040002;
+        public static final int stat_carbon_poll=0x7f04000d;
+        public static final int stat_city_usage=0x7f04000c;
+        public static final int stat_pot_solar=0x7f040007;
+        public static final int stat_pot_solar_image=0x7f040009;
+        public static final int stat_pot_wind=0x7f04000b;
+        public static final int stat_pot_wind_image=0x7f04000a;
         public static final int terrain=0x7f040003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int map_popup_layout=0x7f030001;
+        public static final int stats_calculator_layout=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f070000;
+        public static final int activity_main=0x7f080000;
     }
     public static final class string {
         public static final int app_name=0x7f050011;
@@ -235,11 +254,11 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
     public static final class styleable {
         /** Attributes that can be used with a MapAttrs.
--- src/com/sac/energyhunter/MainActivity.java
+++ src/com/sac/energyhunter/MainActivity.java
@@ -1,42 +1,73 @@
 package com.sac.energyhunter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+import com.google.android.gms.internal.af.b;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
 
-public class MainActivity extends Activity {
-  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-  static final LatLng KIEL = new LatLng(53.551, 9.993);
+public class MainActivity extends Activity implements OnMarkerClickListener{
+  
+  static final LatLng MELBOURNE = new LatLng(-37.814107, 144.96328);
   private GoogleMap map;
-
+  private Button infoBtn;
+  
+  private Marker curLocMark;
+  private int posSolarEnergy;
+  private int posWindEnergy;
+  private int posEUsage;
+  private int posCarbEmis;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
         .getMap();
-    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-        .title("Hamburg"));
-    Marker kiel = map.addMarker(new MarkerOptions()
-        .position(KIEL)
-        .title("Kiel")
-        .snippet("Kiel is cool")
-        .icon(BitmapDescriptorFactory
-            .fromResource(R.drawable.ic_launcher)));
+    infoBtn = (Button)findViewById(R.id.map_more_info_btn);
+    infoBtn.setOnClickListener(new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			onMarkerClick(curLocMark);
+		}
+	});
+    queryBackend();
+    zoomToCoords(MELBOURNE, "Melbourne");
+  }
+  
+  private void queryBackend(){
+	  // code to get the information from the backend
+	  // just placeholders:
+	  posSolarEnergy = 8;
+	  posWindEnergy = 8;
+	  posEUsage = 100;
+	  posCarbEmis = 6;
+  }
+  
+  //use to move the camera to specified LatLng
+  public void zoomToCoords(LatLng coords, String cityName){
+		curLocMark = map.addMarker(new MarkerOptions().position(coords)
+				.title(cityName));
+	    // Move the camera instantly to coords with a zoom of 15.
+	    map.moveCamera(CameraUpdateFactory.newLatLngZoom(coords, 15));
 
-    // Move the camera instantly to hamburg with a zoom of 15.
-    map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-
-    // Zoom in, animating the camera.
-    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	    // Zoom in, animating the camera.
+	    map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	    
   }
 
   @Override
@@ -44,5 +75,33 @@ public class MainActivity extends Activity {
     getMenuInflater().inflate(R.menu.activity_main, menu);
     return true;
   }
+  
+  /* *
+   * For bundle put in following info:
+   * - latitude of location (double, "Latitude")
+   * - longitude of location (double, "Longitude")
+   * - solar energy (mW/h (or kW/h, sorry im clueless)) (int, "SolarEnergy")
+   * - wind energy (mW/h (or kW/h, again, im clueless)) (int, "WindEnergy")
+   * - total current city energy usage (kW/h or whatever unit) (int, "TotalEUsage")
+   * - carbon emissions from city energy usage (tonnes) (int, "CarbonEmissions")
+   */
+  public void goToStatCalculator(Bundle bundle){
+	  Intent statCalcIntent = new Intent(this, StatsCalculator.class);
+	  statCalcIntent.putExtras(bundle);
+	  startActivity(statCalcIntent);
+  }
+
+	@Override
+	public boolean onMarkerClick(Marker marker) {
+		Bundle b = new Bundle();
+		b.putDouble("Latitude", marker.getPosition().latitude);
+		b.putDouble("Longitude", marker.getPosition().longitude);
+		b.putInt("SolarEnergy", posSolarEnergy);
+		b.putInt("WindEnergy", posWindEnergy);
+		b.putInt("TotalEUsage", posEUsage);
+		b.putInt("CarbonEmissions", posCarbEmis);
+		goToStatCalculator(b);
+		return false;
+	}
 
 } 
